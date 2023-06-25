518e9b7522dd6a5099231339f7da6efb1dd3ecdc
Jordan Masone <jordanmasone1@gmail.com>
2023-06-25 15:52:48 -0400

    Section 2

--- practice/src/app/app.component.ts
+++ practice/src/app/app.component.ts
@@ -5,7 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'my practice app';
-  name = 'Jordan';
-}
+export class AppComponent {}
