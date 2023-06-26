654c831a2955257cf14350bdf451caa91ffdfd87
Jordan Masone <jordanmasone1@gmail.com>
2023-06-26 18:58:56 -0400

    Added components for assignment one.

--- practice/src/app/app.module.ts
+++ practice/src/app/app.module.ts
@@ -2,13 +2,17 @@ import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {AppComponent} from './app.component';
 import {ServerComponent} from "./server/server.component";
-import { ServersComponent } from './servers/servers.component';
+import {ServersComponent} from './servers/servers.component';
+import {WarningAlertComponent} from "./warningAlert/warningAlert.component";
+import {SuccessAlertComponent} from "./successAlert/successAlert.component";
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
-    ServersComponent
+    ServersComponent,
+    WarningAlertComponent,
+    SuccessAlertComponent
   ],
   imports: [BrowserModule],
   providers: [],
---
+++ practice/src/app/successAlert/successAlert.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core"
+
+@Component({
+  selector: "app-success-alert",
+  templateUrl: "./successAlert.component.html",
+  styles: [`
+    h3 {
+      color: #00FF00
+    }
+  `]
+})
+export class SuccessAlertComponent {}
